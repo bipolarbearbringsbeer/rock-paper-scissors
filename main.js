7686b8671ed7bebf4b0f593a11fd85a6a9c83264
bipolarbearbringsbeer <bipolarbearbringsbeer@gmail.com>
2024-06-04 14:49:36 +0300

    Add user choice functionality

--- main.js
+++ main.js
@@ -1,7 +1,25 @@
-const variation = ['rock', 'paper', 'scissors'];
+const variation = ["rock", "paper", "scissors"];
 
 function getComputerChoice() {
     let computerChoice = variation[Math.floor(Math.random() * variation.length)];
 
     return computerChoice;
 }
+
+function getUserChoice() {
+    let wrongInput = true;
+
+    while (wrongInput) {
+        let userChoice = prompt("Choose rock, paper or scissors: ").toLowerCase();
+
+        if (
+            userChoice === "rock" ||
+            userChoice === "paper" ||
+            userChoice === "scissors"
+        ) {
+            return userChoice;
+        }
+    }
+}
+
+console.log(getUserChoice());
