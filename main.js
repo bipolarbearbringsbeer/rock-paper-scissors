016b773275964232d3061f5762bea0d845455ca4
bipolarbearbringsbeer <bipolarbearbringsbeer@gmail.com>
2024-06-12 20:35:55 +0300

    Add score block

--- main.js
+++ main.js
@@ -1,56 +1,62 @@
 const variation = ["rock", "paper", "scissors"];
-
-function playGame() {
-    let userScore = 0;
-    let computerScore = 0;
-
-    // UI implementation
-    const body = document.querySelector("body");
-    const buttonsBlock = document.createElement("div");
-    const rockButton = document.createElement("button");
-    const paperButton = document.createElement("button");
-    const scissorsButton = document.createElement("button");
-
-    buttonsBlock.classList.add("buttons-block");
-    rockButton.textContent = "Rock";
-    paperButton.textContent = "Paper";
-    scissorsButton.textContent = "Scissors";
-
-    buttonsBlock.append(rockButton, paperButton, scissorsButton);
-    body.append(buttonsBlock);
-
-    rockButton.addEventListener("click", () => {
-        playRound("rock");
-    });
-    paperButton.addEventListener("click", () => {
-        playRound("paper");
-    });
-    scissorsButton.addEventListener("click", () => {
-        playRound("scissor");
-    })
+let userScore = 0;
+let computerScore = 0;
 
 
-    function playRound(humanChoice) {
-        let computerChoice = getComputerChoice();
+// UI implementation
+const body = document.querySelector("body");
+const buttonsBlock = document.createElement("div");
+const rockButton = document.createElement("button");
+const paperButton = document.createElement("button");
+const scissorsButton = document.createElement("button");
+const scoreBlock = document.createElement("div");
+const userScoreParagraph = document.createElement("p");
+const computerScoreParagraph = document.createElement("p");
 
-        if (computerChoice === humanChoice) {
-            console.log("It's draw!!! No one is winner! 🤔");
-        }
-        else if (
-            (computerChoice === "rock" && humanChoice === "scissors") ||
-            (computerChoice === "paper" && humanChoice === "rock") ||
-            (computerChoice === "scissors" && humanChoice === "paper")
-        ) {
-            console.log(`Computer chose ${computerChoice}, you chose ${humanChoice} and lose round! He-he-he 🤣`);
-            computerScore += 1;
-        }
-        else {
-            console.log(`Computer chose ${computerChoice}, you chose ${humanChoice}. You win, whatever 😞`);
-            userScore += 1;
-        }
+buttonsBlock.classList.add("buttons-block");
+scoreBlock.classList.add("score-block");
+rockButton.textContent = "Rock";
+paperButton.textContent = "Paper";
+scissorsButton.textContent = "Scissors";
+userScoreParagraph.textContent = `User Score: ${userScore}`;
+computerScoreParagraph.textContent = `Computer Score: ${computerScore}`;
+
+buttonsBlock.append(rockButton, paperButton, scissorsButton);
+scoreBlock.append(userScoreParagraph, computerScoreParagraph);
+body.append(buttonsBlock, scoreBlock);
+
+rockButton.addEventListener("click", () => {
+    playRound("rock");
+});
+paperButton.addEventListener("click", () => {
+    playRound("paper");
+});
+scissorsButton.addEventListener("click", () => {
+    playRound("scissor");
+})
+
+
+function playRound(humanChoice) {
+    let computerChoice = getComputerChoice();
+
+    if (computerChoice === humanChoice) {
+        console.log("It's draw!!! No one is winner! 🤔");
     }
-
-    printResult(userScore, computerScore);
+    else if (
+        (computerChoice === "rock" && humanChoice === "scissors") ||
+        (computerChoice === "paper" && humanChoice === "rock") ||
+        (computerChoice === "scissors" && humanChoice === "paper")
+    ) {
+        console.log(`Computer chose ${computerChoice}, you chose ${humanChoice} and lose round! He-he-he 🤣`);
+        computerScore += 1;
+    }
+    else {
+        console.log(`Computer chose ${computerChoice}, you chose ${humanChoice}. You win, whatever 😞`);
+        userScore += 1;
+    }
+    userScoreParagraph.textContent = `User Score: ${userScore}`;
+    computerScoreParagraph.textContent = `Computer Score: ${computerScore}`;
+    console.log(userScore, computerScore);
 }
 
 function getComputerChoice() {
@@ -59,23 +65,6 @@ function getComputerChoice() {
 
     return computerChoice;
 }
-
-// function getUserChoice() {
-//     let wrongInput = true;
-
-//     // Check for right user input
-//     while (wrongInput) {
-//         let userChoice = prompt("Choose rock, paper or scissors: ").toLowerCase();
-
-//         if (
-//             userChoice === "rock" ||
-//             userChoice === "paper" ||
-//             userChoice === "scissors"
-//         ) {
-//             return userChoice;
-//         }
-//     }
-// }
 
 function printResult(userScore, computerScore) {
     if (userScore > computerScore) {
@@ -89,4 +78,4 @@ function printResult(userScore, computerScore) {
     }
 }
 
-playGame();
+// playGame();
